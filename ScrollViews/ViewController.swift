8c4ec1803716e7a0b0f545cb17b3038d4580b5d7
Lane Faison <lane.faison@icloud.com>
2017-05-11 11:15:06 -0600

    ScrollView before Gesture Add

--- ScrollViews/ViewController.swift
+++ ScrollViews/ViewController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]() // initializes an empty array of images
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
+        let scrollHeight = scrollView.frame.size.height
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = (UIImageView(image: image))
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth/2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX-75, y: (scrollHeight / 2)-75, width: 150, height: 150)
+        }
+        
+        scrollView.clipsToBounds = false // This shows the next item coming in on scroll
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
 
 
